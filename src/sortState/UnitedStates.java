8187634acb4e634314783fe7634173d19e0c50a0
ags3780 <hammerhead12as@icloud.com>
2023-02-22 21:01:54 -0600

    final

--- src/sortState/UnitedStates.java
+++ src/sortState/UnitedStates.java
@@ -33,16 +33,56 @@ public class UnitedStates
 	 * by the state's name
 	 */
 	public void sortStates(int front, int back) {
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		if (l < r) {
+	        int m = l + (r - l) / 2;
+	        mergeSort(arr, l, m);
+	        mergeSort(arr, m + 1, r);
+	        merge(arr, l, m, r);
+	    }
 	}
+
+	public static void merge(int[] arr, int l, int m, int r) {
+	    int n1 = m - l + 1;
+	    int n2 = r - m;
+	    int[] L = new int[n1];
+	    int[] R = new int[n2];
+	    for (int i = 0; i < n1; ++i)
+	    	L[i] = arr[l + i];
+	    for (int j = 0; j < n2; ++j)
+	    		R[j] = arr[m + 1 + j];
+	   int i = 0, j = 0;
+	   int k = l;
+	    while (i < n1 && j < n2) {
+	    	if (L[i] <= R[j]) {
+	        		arr[k] = L[i];
+	        			i++;
+	        } else {
+	        		arr[k] = R[j];
+	            j++;
+	        }
+	        k++;
+	    }
+	    while (i < n1) {
+	        arr[k] = L[i];
+	        	i++;
+	        k++;
+	    }
+	    while (j < n2) {
+	        arr[k] = R[j];
+	        	j++;
+	        k++;
+	    }
+	}
+
+		
+		
+		
+		
+		
+		
+		
+		
+	
 	
 	
 	/*
@@ -52,14 +92,37 @@ public class UnitedStates
 	 * by the state's capital
 	 */
 	public void sortCapitals(int low, int high) {
+		if (low < high) {
+            int pivotIndex = partition(states, low, high);
+            quickSort(states, low, pivotIndex-1);
+            quickSort(states, pivotIndex+1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+	
 		
 		
 		
 		
 		
 		
-		
-	}
+	
 	
 	
 	
